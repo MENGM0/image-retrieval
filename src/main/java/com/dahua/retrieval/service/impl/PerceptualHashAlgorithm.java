89bf9e6811c0129c0a4177317c8d9391935f7366
qingsong <303514180@qq.com>
2024-09-06 00:26:26 +0800

    1. 抽取图片指纹存取json；2. 相似度Top20计算并展示图片；3.计算每张图片的Top50存入json。

--- src/main/java/com/dahua/retrieval/service/impl/PerceptualHashAlgorithm.java
+++ src/main/java/com/dahua/retrieval/service/impl/PerceptualHashAlgorithm.java
@@ -2,22 +2,25 @@ package com.dahua.retrieval.service.impl;
 
 import com.dahua.retrieval.algorithm.FingerPrint;
 import com.dahua.retrieval.service.SimilarityCalculate;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
-import static com.dahua.retrieval.constant.Constant.AI_CHALLENGER_CAPTION_VAL_ROOT_PATH;
+import static com.dahua.retrieval.constant.Constant.*;
 import static com.dahua.retrieval.utils.FileUtil.calcMemory;
 import static com.dahua.retrieval.utils.FileUtil.getFileAbsolutePaths;
 
@@ -156,24 +159,148 @@ public class PerceptualHashAlgorithm implements SimilarityCalculate {
 
 //        return combinedImage;
     }
+    
+    public static List<JSONObject> saveImageFingerPrintJson(String rootPath, String jsonPath){
+        long startTime = System.currentTimeMillis();
+        // 获取处理器的核心数
+        int corePoolSize = Runtime.getRuntime().availableProcessors();
+        System.out.println("corePoolSize: " + corePoolSize);
+        // 图片路径数组
+        List<String> imageList = getFileAbsolutePaths(rootPath);
+        // 创建一个固定大小的线程池
+        ExecutorService executor = Executors.newFixedThreadPool(corePoolSize);
+        Map<String, FingerPrint> path2FingerMap = new HashMap<>();
+        List<JSONObject> featureJson = new ArrayList<>();
+        for (String imagePath : imageList) {
+            // 提交读取图片的任务到线程池
+            executor.submit(() -> {
+                try {
+                    File imageFile = new File(imagePath);
+                    FingerPrint fp = new FingerPrint(ImageIO.read(imageFile));
+                    JSONObject obj = new JSONObject();//创建JSONObject对象
+                    obj.put("feature_fp", fp.toString());
+                    obj.put("image_id", imageFile.getName());
+                    featureJson.add(obj);
 
-    public static void main(String[] args) throws IOException {
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+        // 关闭线程池，不接受新任务，等待已提交的任务完成
+        executor.shutdown();
+        try {
+            // 等待直到所有任务完成或超时
+            executor.awaitTermination(1, TimeUnit.HOURS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        long sumTime = (System.currentTimeMillis() - startTime) / 1000;
+        System.out.println("==================读取图片" + featureJson.size() + "张==================");
+        System.out.println("==================运行时间：" + sumTime + "秒==================");
 
-        // 单线程读取
-//        readFingerPrintListSingleThread();
-        // 多线程读取
-        Map<String, FingerPrint> path2FingerMap = readFingerPrintListMultiThread();
+        // 将List转换为JSONArray
+        JSONArray jsonArray = new JSONArray(featureJson);
+        // 写入文件
+        try (FileWriter file = new FileWriter(jsonPath)) {
+            file.write(jsonArray.toString(4)); // 4是缩进的空格数
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return featureJson;
+    }
 
-        calcMemory();
+    public static Map<String, FingerPrint> loadJsonImageFeatureFP(String jsonPath){
+        Map<String, FingerPrint> fingerPrintMap = new HashMap<>();
+        try {
+            // 读取文件内容到字符串
+            String content = new String(Files.readAllBytes(Paths.get(jsonPath)));
 
-        int compareNum = 2;
+            // 将字符串转换为JSONArray
+            JSONArray jsonArray = new JSONArray(content);
+
+            // 将JSONArray转换为List<JSONObject>
+            List<JSONObject> jsonObjectList = new ArrayList<>();
+            for (int i = 0; i < jsonArray.length(); i++) {
+//                jsonObjectList.add(jsonArray.getJSONObject(i));
+                JSONObject obj = jsonArray.getJSONObject(i);
+                fingerPrintMap.put(obj.get("image_id").toString(), new FingerPrint(obj.get("feature_fp").toString()));
+//                System.out.println(obj);
+            }
+
+            // 打印JSON对象列表
+//            jsonObjectList.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//        System.out.println(fingerPrintMap);
+        return fingerPrintMap;
+    }
+
+    // 跑demo
+    public static void createImageGridWithLabels(List<Map.Entry<String, Float>> imageEntries, int columns, int imageSize, int textOffset) throws IOException {
+        String imageId = imageEntries.get(1).getKey();
+        int rows = (int) Math.ceil((double) imageEntries.size() / columns);
+
+        // 计算整个网格的宽度和高度（包括文本高度）
+        int gridWidth = columns * imageSize;
+        int gridHeight = rows * (imageSize + textOffset);
+
+        // 创建一个新的图像，用于绘制所有图片
+        BufferedImage gridImage = new BufferedImage(gridWidth, gridHeight, BufferedImage.TYPE_INT_RGB);
+        Graphics2D g2d = gridImage.createGraphics();
+
+        // 设置背景色
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(0, 0, gridWidth, gridHeight);
+
+        int index = 0;
+        for (int row = 0; row < rows; row++) {
+            for (int col = 0; col < columns; col++) {
+                if (index < imageEntries.size()) {
+                    Map.Entry<String, Float> entry = imageEntries.get(index);
+                    // 读取图片
+                    BufferedImage image = ImageIO.read(new File(AI_CHALLENGER_CAPTION_VAL_ROOT_PATH + "\\" + entry.getKey()));
+                    // 调整图片大小
+                    BufferedImage resizedImage = new BufferedImage(imageSize, imageSize, BufferedImage.TYPE_INT_RGB);
+                    Graphics2D g2 = resizedImage.createGraphics();
+                    g2.drawImage(image.getScaledInstance(imageSize, imageSize, Image.SCALE_SMOOTH), 0, 0, null);
+                    g2.dispose();
+
+                    // 绘制图片到网格
+                    g2d.drawImage(resizedImage, col * imageSize, row * (imageSize + textOffset), null);
+
+                    // 绘制分数
+                    g2d.setColor(Color.BLACK);
+                    g2d.setFont(new Font("Arial", Font.PLAIN, 12));
+                    String label = String.valueOf(entry.getValue());
+                    FontMetrics metrics = g2d.getFontMetrics();
+                    int x = col * imageSize + (imageSize - metrics.stringWidth(label)) / 2;
+                    int y = row * (imageSize + textOffset) + imageSize + textOffset - metrics.getDescent();
+                    g2d.drawString(label, x, y);
+
+                    index++;
+                }
+            }
+        }
+
+        g2d.dispose();
+
+        // 保存最终的图片
+        ImageIO.write(gridImage, "png", new File(ACC_MERGE_RESULT_DEMO_PATH + "\\" + imageId));
+    }
+    public static void compareFpPlot(Map<String, FingerPrint> fingerPrintMap) throws IOException {
+//        int compareNum = 2;
+        int compareNum = fingerPrintMap.size();
         // 获取前10个元素
-        List<Map.Entry<String, FingerPrint>> first10Entries = new ArrayList<>(path2FingerMap.entrySet())
+        List<Map.Entry<String, FingerPrint>> first10Entries = new ArrayList<>(fingerPrintMap.entrySet())
                 .stream()
                 .limit(compareNum)
                 .collect(Collectors.toList());
+        System.out.println("first10Entries: " +  first10Entries);
         long startTime = System.currentTimeMillis();
-
         for (Map.Entry<String, FingerPrint> entry1: first10Entries){
             // 比对图片
             String ip1 = entry1.getKey();
@@ -186,20 +313,105 @@ public class PerceptualHashAlgorithm implements SimilarityCalculate {
 //                }
 //            }
             // 比对
-            Map<String, Float> res = fp1.batchCompare(path2FingerMap);
+            Map<String, Float> res = fp1.batchCompare(fingerPrintMap);
             // 按照值从大到小排序并取前20个元素
             List<Map.Entry<String, Float>> top20Entries = res.entrySet().stream()
                     .sorted(Map.Entry.<String, Float>comparingByValue().reversed())
                     .limit(20)
                     .collect(Collectors.toList());
-            plotSimilarImageList(ip1, top20Entries, 5);
-
+            createImageGridWithLabels(top20Entries, 5, 100, 20);
             System.out.println(top20Entries);
-            System.out.println("比对" + first10Entries.size() + "张图片，运行时间" + (System.currentTimeMillis() - startTime) + "毫秒");
         }
+        System.out.println("比对" + first10Entries.size() + "张图片，运行时间" + (System.currentTimeMillis() - startTime) + "毫秒");
+    }
 
-        long sumTime = (System.currentTimeMillis() - startTime) ;
-        System.out.println("==================读取图片" + first10Entries.size() + "张==================");
-        System.out.println("==================运行时间：" + sumTime + "毫秒==================");
+    /**
+     * 跑结果
+     *
+     * [
+     *      {"image_id": xxx, "top50": [{"image_id": xxx, "level": yyy}, ..., {"image_id": xxx, "level": yyy}]},
+     *      {"image_id": xxx, "top50": [{"image_id": xxx, "level": yyy}, ..., {"image_id": xxx, "level": yyy}]},
+     *      ...
+     * ]
+     */
+    public static void compareFpJson(Map<String, FingerPrint> fingerPrintMap, String savePath) throws IOException {
+//        int compareNum = 2;
+        int compareNum = fingerPrintMap.size();
+        // 获取前10个元素
+        List<Map.Entry<String, FingerPrint>> first10Entries = new ArrayList<>(fingerPrintMap.entrySet())
+                .stream()
+                .limit(compareNum)
+                .collect(Collectors.toList());
+//        System.out.println("first10Entries: " +  first10Entries);
+        long startTime = System.currentTimeMillis();
+        List<JSONObject> resultJson = new ArrayList<>();
+        int topN = 50;
+        for (Map.Entry<String, FingerPrint> entry1: first10Entries){
+            // 比对图片
+            String ip1 = entry1.getKey();
+            FingerPrint fp1 = entry1.getValue();
+            // 比对
+            Map<String, Float> res = fp1.batchCompare(fingerPrintMap);
+            // 按照值从大到小排序并取前20个元素
+            List<Map.Entry<String, Float>> topNEntries = res.entrySet().stream()
+                    .sorted(Map.Entry.<String, Float>comparingByValue().reversed())
+                    .limit(topN)
+                    .collect(Collectors.toList());
+
+            JSONObject obj1 = new JSONObject();//创建JSONObject对象
+            List<JSONObject> topNJson = new ArrayList<>();
+            for (int i = 1; i < topN; i++){
+                JSONObject obj2 = new JSONObject();//创建JSONObject对象
+                obj2.put("level", topNEntries.get(i).getValue());
+                obj2.put("image_id", topNEntries.get(i).getKey());
+                topNJson.add(obj2);
+            }
+            obj1.put("image_id", topNEntries.get(0).getKey());
+            obj1.put("topN", topNJson);
+            resultJson.add(obj1);
+        }
+        System.out.println("比对" + first10Entries.size() + "张图片，运行时间" + (System.currentTimeMillis() - startTime) + "毫秒");
+        System.out.println("平均运行时间" + (System.currentTimeMillis() - startTime) / 1000/ first10Entries.size() + "秒" );
+
+        startTime = System.currentTimeMillis();
+        // 将List转换为JSONArray
+        JSONArray jsonArray = new JSONArray(resultJson);
+        // 写入文件
+        try (FileWriter file = new FileWriter(savePath)) {
+            file.write(jsonArray.toString(4)); // 4是缩进的空格数
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("写入" + first10Entries.size() + "张图片，运行时间" + (System.currentTimeMillis() - startTime) / 1000 + "秒");
+        System.out.println("平均写入时间" + (System.currentTimeMillis() - startTime) / 1000/ first10Entries.size() + "秒" );
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        // 单线程读取
+//        readFingerPrintListSingleThread();
+        // 多线程读取
+//        Map<String, FingerPrint> path2FingerMap = readFingerPrintListMultiThread();
+
+        // 1. 抽取所有图片指纹信息，保存到json
+//        saveImageFingerPrintJson(AI_CHALLENGER_CAPTION_VAL_ROOT_PATH, ACC_FEATURE_FINGER_PRINT_JSON_PATH);
+
+        // 2. 跑demo：计算图片的top20相似图片，合并相似图片及其相似度，保存到一张图片内
+//        Map<String, FingerPrint> fingerPrintMap = loadJsonImageFeatureFP(ACC_FEATURE_FINGER_PRINT_JSON_PATH);
+//        compareFpPlot(fingerPrintMap);
+
+        // 3. 跑结果：计算图片的top50【计算量：3w*3w】
+        Map<String, FingerPrint> fingerPrintMap = loadJsonImageFeatureFP(ACC_FEATURE_FINGER_PRINT_JSON_PATH);
+        compareFpJson(fingerPrintMap, AI_CHALLENGER_CAPTION_VAL_ROOT_PATH);
+
+
+
+        // 4. 跑结果，demo
+//        saveImageFingerPrintJson(AI_CHALLENGER_CAPTION_VAL_ROOT_PATH2, ACC_FEATURE_FINGER_PRINT_JSON_PATH2);
+//        Map<String, FingerPrint> fingerPrintMap = loadJsonImageFeatureFP(ACC_FEATURE_FINGER_PRINT_JSON_PATH2);
+//        compareFpJson(fingerPrintMap, ACC_FEATURE_FINGER_PRINT_COMPARE_RESULT_JSON_PATH2);
+
     }
 }

f266dc5b6a05be3b912f72abdfb554360ae76f96
303514180@qq.com <303514180@qq.com>
2024-09-25 01:31:27 +0800

    service实现类

--- src/main/java/com/dahua/retrieval/service/impl/ImageServiceImpl.java
+++ src/main/java/com/dahua/retrieval/service/impl/ImageServiceImpl.java
@@ -1,6 +1,10 @@
 package com.dahua.retrieval.service.impl;
 
+import com.dahua.retrieval.entity.Image;
 import com.dahua.retrieval.service.ImageService;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public class ImageServiceImpl implements ImageService {
     @Override
@@ -11,5 +15,40 @@ public class ImageServiceImpl implements ImageService {
     @Override
     public void download(String imageId) {
 
+    }
+
+    @Override
+    public Image uploadImage(MultipartFile imageFile, String description, String[] tags) {
+        return null;
+    }
+
+    @Override
+    public List<Image> searchByImage(MultipartFile imageFile) {
+        return List.of();
+    }
+
+    @Override
+    public List<Image> getImagesByTag(String tag) {
+        return List.of();
+    }
+
+    @Override
+    public List<Image> searchImagesByKeyword(String keyword) {
+        return List.of();
+    }
+
+    @Override
+    public Image getImageById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void deleteImage(Long id) {
+
+    }
+
+    @Override
+    public Image updateImage(Long id, String description, String[] tags) {
+        return null;
     }
 }
